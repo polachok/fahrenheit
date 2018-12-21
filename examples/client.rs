587cf1a990c304a5acdcc9fa6a4161be10f59f1e
Alexander Polakov <plhk@sdf.org>
2018-12-22 00:20:49 +0300

    example/client: cast to string at the end
    Fixes #16

--- examples/client.rs
+++ examples/client.rs
@@ -8,16 +8,16 @@ use futures::io::{AsyncReadExt, AsyncWriteExt};
 async fn http_get(addr: &str) -> Result<String, std::io::Error> {
     let mut conn = AsyncTcpStream::connect(addr)?;
     let _ = await!(conn.write_all(b"GET / HTTP/1.0\r\n\r\n"))?;
-    let mut page = String::new();
+    let mut page = Vec::new();
     loop {
         let mut buf = vec![0; 128];
         let len = await!(conn.read(&mut buf))?;
         if len == 0 {
             break;
         }
-        let res = String::from_utf8_lossy(&buf[..len]);
-        page += &res;
+        page.extend_from_slice(&buf[..len]);
     }
+    let page = String::from_utf8_lossy(&page).into();
     Ok(page)
 }
 
